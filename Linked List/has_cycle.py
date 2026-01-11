933cedefdc393735184612995951b80a00ba93c9
Culabolso <marcusvinicius0.0023@gmail.com>
2026-01-11 00:37:20 -0300

    add hasCycle function to detect cycles in linked list

---
+++ Linked List/has_cycle.py
@@ -0,0 +1,14 @@
+from linkeed_lists import LinkeedList
+
+
+def hasCycle(head) -> bool:
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+
+    return False
